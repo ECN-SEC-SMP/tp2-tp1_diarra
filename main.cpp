b77de08879771f8940f090770315a30ae46ceef5
Diarra <diopdiarra285@gmail.com>
2025-10-14 12:03:39 +0200

    Levee des exeptions

--- main.cpp
+++ main.cpp
@@ -8,28 +8,56 @@ int main() {
     cout << "La vitesse max du vehicule est: " << v.getVitesseMax() << endl;
 
     // Test de mettreEnPanne
+    try{
     v.mettreEnPanne(0.3); // Devrait mettre en panne légère
-    cout << "L'etat du vehicule est: " << v.getEtat() << endl;
+    cout << "L'etat du vehicule est: " << v.getEtat() << endl;}
+    catch (const string& message) {
+        cerr << "Exception attrapee: " << message<< endl;
+    }
 
-    v.mettreEnPanne(0.7); // Devrait mettre en panne sévère
-    cout << "L'etat du vehicule est: " << v.getEtat() << endl;
+    try {
+        v.mettreEnPanne(0.7); // Devrait mettre en panne sévère
+        cout << "L'etat du vehicule est: " << v.getEtat() << endl;
+    } catch (const string& message) {
+        cerr << "Exception attrapee: " << message<< endl;
+    }
 
     // Test de demarrer
-    v.demarrer();
-    cout << "L'etat du vehicule apres demarrage est: " << v.getEtat() << endl;
-    
+    try {
+        v.demarrer();
+        cout << "L'etat du vehicule apres demarrage est: " << v.getEtat() << endl;
+    } catch (const string& message) {
+        cerr << "Exception attrapee: " << message<< endl;
+    }
+
     // Test de arreter
-    v.arreter();
-    cout << "L'etat du vehicule apres arret est: " << v.getEtat() << endl;
-    cout << "La vitesse du vehicule apres arret est: " << v.getVitesse() << endl;
+    try {
+        v.arreter();
+        cout << "L'etat du vehicule apres arret est: " << v.getEtat() << endl;
+        cout << "La vitesse du vehicule apres arret est: " << v.getVitesse() << endl;
+    } catch (const string& message) {
+        cerr << "Exception attrapee: " << message<< endl;
+    }
+
+    //Test de accelerer
+    try {
+        v.accelerer(30);
+        cout << "La vitesse du vehicule apres acceleration est: " << v.getVitesse() << endl;
+    } catch (const string& e) {
+        cerr<< "Exeption attrapee: " << e << endl;
+    }
 
     // Test de depanner
+    try {
     v.mettreEnPanne(0.4); // Met en panne légère
     v.depanner();
-    cout << "L'etat du vehicule apres depannage est: " << v.getEtat() << endl;
+    cout << "L'etat du vehicule apres depannage est: " << v.getEtat() << endl;}
+    catch (const string& message) {
+        cerr << "Exception attrapee: " << message<< endl;
+    }       
 
     // Affichage de l'état complet du véhicule
-    // cout << v << endl;
+    cout << v << endl;
 
     return 0;
 }
